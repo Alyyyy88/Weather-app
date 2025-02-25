3ec8ea567cf5eddc53f3c8e158525f5f2aff74e2
Alyyyy88 <ali_20235078@fci.helwan.edu.eg>
2025-02-25 21:15:36 +0200

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+// i need when i click on the search button it gives me the temp and change the picture of the weather
+// get the wind desc 
+// search by name of city
+
+const btn = document.querySelector("#search-btn");
+
+
+// btn.addEventListener("click",function(e){
+//   e.preventDefault();
+//   console.log("CLIECK ");
+// })
+
+
+// get API
+
+const api_key = "1be9b0b3b3b45d17a858767c32e1f151";
+
+const url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={1be9b0b3b3b45d17a858767c32e1f151}"
+
+https://api.openweathermap.org/data/2.5/weather?q={newyork}&appid={1be9b0b3b3b45d17a858767c32e1f151}

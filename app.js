85f02eaeacc26b4ccc49e7be40a8475d67a0022a
Alyyyy88 <ali_20235078@fci.helwan.edu.eg>
2025-02-26 22:29:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ const btn = document.querySelector("#search-btn");
 
 
 // get API
+const url = "http://api.weatherapi.com/v1/current.json?key=3fec5efc87c44a3b933201819252602&q=cairo";
 
-const api_key = "1be9b0b3b3b45d17a858767c32e1f151";
-
-const url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={1be9b0b3b3b45d17a858767c32e1f151}"
-
-https://api.openweathermap.org/data/2.5/weather?q={newyork}&appid={1be9b0b3b3b45d17a858767c32e1f151}
+console.log(url);
